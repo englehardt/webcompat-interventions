269da28af8ac71b46c3a15788e65c1211c91f28e
englehardt <senglehardt@mozilla.com>
2020-11-13 17:49:35 -0500

    Fix some bugs

--- bug_1665035/fix.js
+++ bug_1665035/fix.js
@@ -1,7 +1,18 @@
 console.info(
   "window.cookieEnabled has been overwritten for compatibility reasons. See https://bugzilla.mozilla.org/show_bug.cgi?id=1665035 for details."
 );
+
 Object.defineProperty(window.navigator.wrappedJSObject, 'cookieEnabled', {
   value: true,
   writable: false
 });
+
+if (navigator.platform.includes("Linux")) {
+  console.info(
+    "navigator.platform has been overwritten for compatibility reasons. See https://bugzilla.mozilla.org/show_bug.cgi?id=1665035 for details."
+  );
+  Object.defineProperty(window.navigator.wrappedJSObject, 'platform', {
+    value: 'Win64',
+    writable: false
+  });
+}
