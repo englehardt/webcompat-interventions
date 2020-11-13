6d2d93d38275aa4246cc940a514d6353c06dc26f
englehardt <senglehardt@mozilla.com>
2020-11-13 16:38:02 -0500

    Updating intervention for new breakage

--- twitterVideoFix.js
+++
@@ -1,7 +0,0 @@
-console.info(
-  "window.indexedDB has been overwritten for compatibility reasons. See https://bugzilla.mozilla.org/show_bug.cgi?id=1641521 for details."
-);
-Object.defineProperty(window.wrappedJSObject, 'indexedDB', {
-  get: undefined,
-  set: undefined
-});
